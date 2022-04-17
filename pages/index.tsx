a022bdcd2e51a2d6e286bf4e57bcd8672f30c6c1
kelvinmuchui <muchuikelvin423@gmail.com>
2022-04-17 18:55:15 +0300

    deployment version 3

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,24 +10,36 @@ interface Props {
 }
 const Home = ({collections}: Props) => {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto flex flex-col min-h-screen
+    py-20 px-10 2xl:px-0">
       <Head>
         <title>Nft-Opensea </title>
         <link rel="icon" href="/favicon.ico" />
 
       </Head>
-      <div className ="flex flex-col h-screen justify-center bg-slate-800 items-center" >
-      <h1 className ="text-indigo-700 text-4xl font-bold">The <span className ="text-orange-500">greatest</span> NFTs</h1>
-      <Link href ="/nft/kelvin">
-      <button className =" mt-10  bg-slate-600  text-white
-            rounded-sm font-bold p-5 border-indigo-700 border-2">
-                Enter Site
-            </button>
-      </Link>
-      
-      </div>
-     
-
+                <h1 className ='mb-10 text-4xl font-extralight'>The{' '} 
+                <span className='font-extrabold underline 
+                decoration-pink-600/50
+                '> KelvinSoi</span>{' '}NFT Market Place</h1>
+                
+        
+        <main className="bg-slate-100 p-10 shadow-xl shadow-rose-400/20">
+          <div className="grid space-x-3 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+            {collections.map((collection) => (
+              <Link href ={`/nft/${collection.slug.current}`}>
+              <div className = "flex flex-col items-center cursor-pointer transition-all duration-200  hover:scale-105">
+                <img
+                className ="h-96 w-60 rounded-2xl object-cover"
+                src ={urlFor(collection.mainImage).url()} alt=""/>
+                <div className="p-5">
+                  <h2 className="text-3xl"> {collection.title}</h2>
+                  <p className ="mt-2 text-sm text-gray-400">{collection.description}</p>
+                </div>
+              </div>
+              </Link>
+            ))}
+          </div>
+        </main>
       
       
     </div>
@@ -44,6 +56,9 @@ export const getServerSideProps: GetServerSideProps  = async () => {
     description,
     nftCollectionName,
     mainImage{
+      asset
+    },
+    previewImage{
       asset
     },
     slug{
--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+import { GetServerSideProps } from 'next';
+import { sanityClient, urlFor } from '../../sanity';
+import { Collection } from '../../typings';
+import Link from 'next/link';
 
-function NFTDropPage() {
+interface Props {
+    collection: Collection ;
+}
+
+function NFTDropPage({collection}: Props) {
     //Auth
     const connectWithMetamask = useMetamask();
     const address = useAddress();
@@ -23,13 +31,13 @@ function NFTDropPage() {
              p-2 rounded-xl">
                 <img className ="w-44 rounded-xl 
                 object-cover lg:h-96 lg:w-72" 
-                 src ="https://links.papareact.com/8sg" 
-                 alt =""/>
+                 src ={urlFor(collection.previewImage).url()} alt=""/> 
+                 
              </div>
              
              <div className ="p-5 space-y-2 text-center">
-                 <h1 className="text-4xl font-bold text-white">Kelvin Apes</h1>
-                 <h2 className ="text-xl text-gray-300">A Collection of Kelvin Apes who live & breathe React!</h2>
+                 <h1 className="text-4xl font-bold text-white">{collection.nftCollectionName}</h1>
+                 <h2 className ="text-xl text-gray-300">{collection.description}</h2>
              </div>
          </div>
 
@@ -38,12 +46,14 @@ function NFTDropPage() {
         <div className= 'flex felx-1 flex-col p-12 lg:col-span-6'> 
             {/* Header */}
             <header className ="flex items-center justify-between">
+                <Link href={"/"}>
                 <h1 className ='w-52 cursor-pointer text-xl
                 font-extralight sm:w-80'>The{' '} 
                 <span className='font-extrabold underline 
                 decoration-pink-600/50
                 '> KelvinSoi</span>{' '}NFT Market Place</h1>
-                <button onClick={()=> address ? disconnect(): connectWithMetamask()} className='rounded-full bg-rose-400 px-4 text-white py-2 text-xs
+                </Link>
+                <button onClick={( )=> address ? disconnect(): connectWithMetamask()} className='rounded-full bg-rose-400 px-4 text-white py-2 text-xs
                 font-bold
                 lg:px-5 lg:py-3 lg:text-base'>
                     {address ? 'Sign Out' : 'Sign In'}</button>
@@ -55,7 +65,7 @@ function NFTDropPage() {
                     You are logged in with wallet {address.substring(0,5)}...{address.substring(address.length-5)}</p>}
             <div className ="mt-10 flex flex-1 flex-col items-center 
             space-y-6 text-center lg:space-y-0 lg:justify-center">
-                <img className ='w-80 object-cover pb-10 lg:w-40' src ="https://links.papareact.com/bdy" alt=""/>
+                <img className ='w-80 object-cover pb-10 lg:w-40' src ={urlFor(collection.mainImage).url()} alt=""/>
 
                 <h1 className ="text-3xl font-bold lg:text-5xl lg:font-extrabold">The Kelvin ape Coding Club | NFT Drop</h1>
                 <p className ="pt-2 text-xl text-green-500"> 13/21 NFT's claimed</p>
@@ -74,3 +84,50 @@ function NFTDropPage() {
 }
 
 export default NFTDropPage
+
+export const getServerSideProps: GetServerSideProps = async ({params}) => {
+    const query = `*[_type == "collection" && slug.current == $id][0]{
+        _id,
+        title,
+        address,
+        description,
+        nftCollectionName,
+        mainImage{
+          asset
+        },
+        previewImage{
+          asset
+        },
+        slug{
+          current
+        },
+        creator->{
+          _id,
+          name,
+          address,
+          slug{
+            current
+          },
+        },
+      
+      }`
+
+      const collection = await sanityClient.fetch(query, {
+          id: params?.id})
+
+
+          if(!collection) {
+              return{
+                notFound: true
+
+              }
+
+        }
+
+        return {
+            props: {
+                collection,
+            }
+        }
+      
+}
